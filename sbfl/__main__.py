55d91db0b4b91d99198922dd05e02b948eeed572
HansolChoe <hansol614@gmail.com>
2022-02-25 14:18:25 +0900

    Make SBFL executable
    Read all gcov files in the directories and print suspicious scores(default dataframe representation).
    This executable is made for defects4cpp.
    
    Implicit Rules
    - Each directory has one and only *.test file.
    - *.test file records the test result ('failed' when test not passed)
    Etc.

---
+++ sbfl/__main__.py
@@ -0,0 +1,72 @@
+import argparse
+from pathlib import Path
+from sbfl.base import SBFL
+from sbfl.utils import gcov_files_to_frame, get_sbfl_scores_from_frame
+
+
+def _get_choices():
+    from inspect import getmembers, isfunction
+    from sbfl import sbfl_formula
+    return [x[0] for x in getmembers(sbfl_formula, isfunction)]
+
+
+def _argparse():
+    parser = argparse.ArgumentParser(prog='sbfl')
+    parser.add_argument('-f',
+                        '--formula',
+                        dest='formula',
+                        nargs=1,
+                        type=str,
+                        choices=_get_choices(),
+                        help='sbfl formula')
+    parser.add_argument('dirs', nargs='+', help='directory to each search for gcov files')
+    return parser.parse_args()
+
+
+def _check_gcov_dirs(gcov_dirs):
+    ret = True
+    for d in gcov_dirs:
+        if not Path(d).exists():
+            print(f'{d} not exists')
+            ret = False
+            continue
+        if not Path(d).is_dir():
+            print(f'{d} is not a directory')
+            ret = False
+            continue
+        glob_test_files = list(Path(d).glob('*.test'))
+        if len(glob_test_files) != 1:
+            print(f'{d} should have one and only test result file')
+            ret = False
+            continue
+    return ret
+
+
+def _get_failing_tests(gcov_dirs):
+    ret = []
+    for d in gcov_dirs:
+        test_file = next(Path(d).glob('*.test'), None)
+        assert(test_file is not None)
+        with open(test_file, 'r') as f:
+            if f.read().rstrip() == 'failed':
+                ret.append(Path(d))
+    return ret
+
+
+def main():
+    args = _argparse()
+    gcov_dirs = []
+    for d in args.dirs:
+        gcov_dirs.extend(Path('.').glob(d))
+    _check_gcov_dirs(gcov_dirs) or exit(1)
+    sbfl = SBFL(formula=args.formula[0])
+    failing_tests = _get_failing_tests(gcov_dirs)
+    gcov_files = {Path(d): list(Path(d).glob('*.gcov')) for d in gcov_dirs}
+    cov_df = gcov_files_to_frame(gcov_files, only_covered=True)
+    sbfl_score = get_sbfl_scores_from_frame(cov_df, sbfl=sbfl, failing_tests=failing_tests)
+    print(sbfl_score)
+
+
+if __name__ == '__main__':
+    main()
+
